e08161d860d5a136cf526c77cb759e1b4d1ce851
Maksim Ermolaev <ermolmak@gmail.com>
2018-10-16 16:24:38 +0300

    operator +

--- Vector.py
+++ Vector.py
@@ -1,4 +1,5 @@
 import math
+import numbers
 
 
 class Vector(object):
@@ -24,6 +25,22 @@ class Vector(object):
             return NotImplemented
         else:
             self._storage[key] = value
+
+    def __add__(self, other):
+        if type(other) == Vector:
+            if len(other) != len(self):
+                raise ValueError("sum operands aren't length equal")
+            return Vector(map(lambda x, y: x + y,
+                              self._storage,
+                              other._storage))
+
+        elif isinstance(other, numbers.Number):
+            return Vector(map(lambda x: x + other, self._storage))
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        return self + other
 
     def matrix_mult(self, matrix):
         pass
