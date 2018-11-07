73cb4d9e2fb9aa7b7107bc21530448fdf48239e4
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-07 22:48:26 +0300

    Transaction: Storage & _ContextManager done

---
+++ task_05/Transaction.py
@@ -0,0 +1,38 @@
+class _ContextManager:
+    def __init__(self, main_storage):
+        self._main_storage = main_storage
+        self._new_storage = {}
+
+    def __getitem__(self, item):
+        try:
+            return self._new_storage[item]
+        except KeyError:
+            return self._main_storage[item]
+
+    def __setitem__(self, key, value):
+        self._new_storage[key] = value
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            return False
+
+        for key, value in self._new_storage.items():
+            self._main_storage[key] = value
+        return True
+
+
+class Storage:
+    def __init__(self, *args, **kwargs):
+        self._storage = dict(*args, **kwargs)
+
+    def __len__(self):
+        return len(self._storage)
+
+    def __getitem__(self, item):
+        return self._storage[item]
+
+    def edit(self):
+        return _ContextManager(self._storage)
