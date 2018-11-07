b56ca7e4cdb0225b90aa629ed2ba149b15c5bd9f
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-07 21:44:14 +0300

    ContextTimer: testing

--- task_05/ContextTimer.py
+++ task_05/ContextTimer.py
@@ -10,3 +10,29 @@ class Timer:
         print('This block has been working for '
               f'{self.stop_time - self.start_time} s')
         return exc_type is None
+
+
+if __name__ == '__main__':
+    def bubble_sort(a):
+        for i in range(len(a) - 2, -1, -1):
+            for j in range(i + 1):
+                if a[j] > a[j + 1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+
+
+    a = list(reversed(range(10)))
+    with Timer():
+        bubble_sort(a)
+
+    a = list(reversed(range(100)))
+    with Timer():
+        bubble_sort(a)
+
+    a = list(reversed(range(1_000)))
+    t = Timer()
+    with t:
+        bubble_sort(a)
+
+    a = list(reversed(range(10_000)))
+    with t:
+        bubble_sort(a)
