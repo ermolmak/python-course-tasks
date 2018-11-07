ffcf2896064ce71be4d4ddc38e124d78ceade638
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-08 01:03:13 +0300

    PhoneNumbers: finished

--- task_05/PhoneNumbers.py
+++ task_05/PhoneNumbers.py
@@ -1,6 +1,25 @@
-CITY_NUMBER = r'[0-9]' \
-              r'(-?[0-9]){1,6}'
+import re
 
-FEDERAL_NUMBER = r'(+?7)?' \
-                 r'((\([0-9]{3}\)-?)|((^|-)[0-9]{3}-)|([0-9]{3}))' \
-                 r'[0-9](-?[0-9]){6}'
+CITY_NUMBER = r'\d' \
+              r'(-?\d){1,6}'
+
+FEDERAL_NUMBER = r'((\+?7)?(\(\d{3}\)-?)|((^|(\+?7-))\d{3}-)|((\+?7)?\d{3}))' \
+                 r'\d(-?\d){6}'
+
+
+def is_city_number(s):
+    res = re.match(CITY_NUMBER, s)
+    return res is not None and len(res.group(0)) == len(s)
+
+
+def is_federal_number(s):
+    res = re.match(FEDERAL_NUMBER, s)
+    return res is not None and len(res.group(0)) == len(s)
+
+
+def is_phone_number(s):
+    if is_city_number(s):
+        return 'city'
+    if is_federal_number(s):
+        return 'federal'
+    return False
