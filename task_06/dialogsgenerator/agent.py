d597bacd18bc2c856309f743a8004905e826e11b
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-15 19:31:51 +0300

    task6: agent.py: random shuffling

--- task_06/dialogsgenerator/agent.py
+++ task_06/dialogsgenerator/agent.py
@@ -1,5 +1,6 @@
 from collections import Generator
 from random import randrange
+from random import shuffle
 
 
 class Agent(Generator):
@@ -8,23 +9,33 @@ class Agent(Generator):
         # Инициализация
         self._texts = kb['text']
         self._name = name
+        self._create_random_queue()
+
+    def _create_random_queue(self):
+        queue = list(range(len(self._texts)))
+        shuffle(queue)
+        self._iter = iter(queue)
 
     def __iter__(self):
-        return self  # TODO
+        return self
 
     def __next__(self):
-        return f'{self._name}: {self._texts[randrange(self._texts.size)]}'  # TODO
+        try:
+            return f'{self._name}: {self._texts[next(self._iter)]}'
+        except StopIteration:
+            self._create_random_queue()
+            return f'{self._name}: {self._texts[next(self._iter)]}'
 
     def send(self, msg):
         # Необходимо написать свой собственный метод send для генератора
-        return next(self)  # TODO: think about it
+        return next(self)
 
     def throw(self, typ=None, val=None, tb=None):
         # Оставляем как есть
         super().throw(typ, val, tb)
 
     def close(self):
-        pass  # TODO
+        pass
 
     def __str__(self):
         # Опишем строковое представление класса
