d097ccc6091f6b2882ab4b83631c2ce12bff44ab
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-15 22:51:48 +0300

    task6: run_generator.py: finished (without -o)

--- task_06/run_generator.py
+++ task_06/run_generator.py
@@ -1,8 +1,10 @@
-from dialogsgenerator import Agent
-from dialogsgenerator import RandomDialog
-
 import argparse
 import sys
+
+import pandas as pd
+
+from dialogsgenerator import Agent
+from dialogsgenerator import RandomDialog
 
 
 def generate(rd, count_dialogs=5):
@@ -43,3 +45,11 @@ if __name__ == "__main__":
         raise ValueError("'count_dialogs' must be positive")
     if args.max_dialog_len <= 0:
         raise ValueError("'max_dialog_len' must be positive")
+
+    clinton = pd.read_csv(clinton_filename)
+    trump = pd.read_csv(trump_filename)
+
+    rd = RandomDialog([Agent(clinton, 'clinton'), Agent(trump, 'trump')],
+                      args.max_dialog_len)
+
+    write(generate(rd, args.count_dialogs))
