8e2ce312c80ba26fc68e25f69b85faadbe9d27d2
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-15 22:22:58 +0300

    task6: run_generator.py: write

--- task_06/run_generator.py
+++ task_06/run_generator.py
@@ -1,3 +1,9 @@
+from dialogsgenerator import Agent
+from dialogsgenerator import RandomDialog
+
+import sys
+
+
 def generate(rd, count_dialogs=5):
     """
     Генерирует count_dialogs диалогов с помощью rd.generate().
@@ -6,11 +12,13 @@ def generate(rd, count_dialogs=5):
     yield from map(lambda i: rd.eval(), range(count_dialogs))
 
 
-def write(dialogs, target):
+def write(dialogs, target=sys.stdout):
     """
     Записывает сгенерированные диалоги dialogs (это объект-генератор) в поток target.
     """
-    pass
+    print(*map(lambda dialog: f'{f"Dialog {dialog[0]}":_^80}\n' +
+                              RandomDialog.dialog_to_str(dialog[1]),
+               enumerate(dialogs)), sep='\n', file=target)
 
 
 if __name__ == "__main__":
