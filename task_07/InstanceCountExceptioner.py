200efb515a9fb3293a2bd256c2880cada93c24e3
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-22 21:59:12 +0300

    task7: InstanceCountExceptioner.py: InstanceCountExceptioner.__new__

--- task_07/InstanceCountExceptioner.py
+++ task_07/InstanceCountExceptioner.py
@@ -7,3 +7,18 @@ class InstanceCountException(Exception):
 
     def __repr__(self):
         return f'InstanceCountException({repr(self.msg)})'
+
+
+_MAX_INSTANCE_COUNT = '__max_instance_count__'
+_INSTANCE_COUNT = '__instance_count__'
+
+
+class InstanceCountExceptioner(type):
+    _default__max_instance_count__ = 1  # singleton
+
+    def __new__(mcs, name, bases, attrs):
+        new_attrs = {_INSTANCE_COUNT: 0}
+        if _MAX_INSTANCE_COUNT not in attrs:
+            new_attrs[_MAX_INSTANCE_COUNT] = mcs._default__max_instance_count__
+
+        return type.__new__(mcs, name, bases, dict(**attrs, **new_attrs))
